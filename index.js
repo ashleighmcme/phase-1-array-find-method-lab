18f9329ebfe3debb29f1245e175aea3b3a9bdfc8
Ashleigh McMahon <ashleighmc@me.com>
2023-11-12 16:59:09 -0700

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+
+
+function superbowlWin(record) {
+    const winningGame = record.find(function(game) {
+       return game.result === "W";
+    });
+    if (winningGame) {
+       return winningGame.year;
+    }
+ }
+ 
+ const record = [
+    { year: 2014, result: "L" },
+    { year: 2015, result: "W" },
+    { year: 2016, result: "L" }
+ ];
+ 
